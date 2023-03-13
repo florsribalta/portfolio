3fe3943911fe5e0de3c583a85283af01d8e072ec
Flors Ribalta <florsribalta1@gmail.com>
2023-03-13 12:45:51 +0100

    Updates

---
+++ precache-manifest.9d6c6175eabacd3d78cc455f71f1db44.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d8222027bd3808e9a6",
+    "url": "/portfolio/static/css/main.35d9e91d.chunk.css"
+  },
+  {
+    "revision": "74d8222027bd3808e9a6",
+    "url": "/portfolio/static/js/main.a3f1754f.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "88b06a93624e498b2ede",
+    "url": "/portfolio/static/css/2.82d2be52.chunk.css"
+  },
+  {
+    "revision": "88b06a93624e498b2ede",
+    "url": "/portfolio/static/js/2.6e7801c6.chunk.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "72ddd566a7926a0b7252abf8db85ffed",
+    "url": "/portfolio/index.html"
+  }
+];
