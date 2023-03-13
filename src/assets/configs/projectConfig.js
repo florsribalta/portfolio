ebdc77fa58b21ab4a59008b1c3915d0f602411dc
Flors Ribalta <florsribalta1@gmail.com>
2023-03-13 12:15:30 +0100

    Index change

--- src/assets/configs/projectConfig.js
+++ src/assets/configs/projectConfig.js
@@ -12,28 +12,23 @@ import React from 'react'
 
 const projectConfig = [
     {
-        id: "project-6",
-        title: "Sklearn Genetic Opt",
+        id: "project-5",
+        title: "Tipologia",
         links: [
             {
                 name: "repo",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt",
+                url: "https://github.com/florsribalta/Tipologia",
                 icon: <AiFillGithub/>,
             },
             {
                 name: "fork",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/fork",
+                url: "https://github.com/florsribalta/Tipologia/fork",
                 icon: <BiGitRepoForked/>,
             },
             {
                 name: "subscription",
-                url: "https://github.com/rodrigo-arenas/Sklearn-genetic-opt/subscription",
+                url: "https://github.com/florsribalta/Tipologia/subscription",
                 icon: <AiFillEye/>,
-            },
-            {
-                name: "docs",
-                url: "https://sklearn-genetic-opt.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
             }
         ],
         image: sklearnGenetic,
@@ -93,107 +88,7 @@ const projectConfig = [
         description: "Graph embeddings for downstream tasks.",
         target: "_blank"
     },
-    {
-        id: "project-3",
-        title: "Kafkaml Anomalies",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/kafkaml-anomaly-detection/subscription",
-                icon: <AiFillEye/>
-            }
-        ],
-        image: kafkaMl,
-        description: "Project for real-time anomaly detection using kafka and python.",
-        target: "_blank"
-    },
-    {
-        id: "project-2",
-        title: "Pyworkforce",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/pyworkforce",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/pyworkforce/subscription",
-                icon: <AiFillEye/>
-            },
-            {
-                name: "docs",
-                url: "https://pyworkforce.readthedocs.io/en/stable/",
-                icon: <ImBook/>,
-            }
-        ],
-        image: "https://miro.medium.com/max/1400/1*UDnhOFK35IbAPx15wkMgVg.jpeg",
-        description: "Standard tools for workforce management, queuing, scheduling, rostering and optimization problems.",
-        target: "_blank"
-    },
-    {
-        id: "project-1",
-        title: "Portfolio Web",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/portfolio",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/portfolio/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/portfolio/subscription",
-                icon: <AiFillEye/>
-            }
-        ],
-        image: portfolio,
-        description: "Source code of my current portfolio web page as data scientist.",
-        target: "_blank"
-    },
-    {
-        id: "project-0",
-        title: "Scikit-Pipes",
-        links: [
-            {
-                name: "repo",
-                url: "https://github.com/rodrigo-arenas/scikit-pipes",
-                icon: <AiFillGithub/>
-            },
-            {
-                name: "fork",
-                url: "https://github.com/rodrigo-arenas/scikit-pipes/fork",
-                icon: <BiGitRepoForked/>
-            },
-            {
-                name: "subscription",
-                url: "https://github.com/rodrigo-arenas/scikit-pipes/subscription",
-                icon: <AiFillEye/>
-            }
-        ],
-        image: scikitPipes,
-        description: "Scikit-Learn practical pre-defined Pipelines Hub.",
-        target: "_blank"
-    }
+
 ]
 
 export default projectConfig
